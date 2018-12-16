96af4a60d321f14ed41398c0d987d4a1af34d89c
SneakyOtterStudio <31124705+SneakyOtterStudio@users.noreply.github.com>
2018-12-16 14:15:01 -0500

    Initial Commit

---
+++ Assets/Art/Saves/Overworld.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Overworld" tilewidth="16" tileheight="16" tilecount="16384" columns="256">
+ <image source="../Art/Overworld.png" trans="ff00ff" width="4096" height="1024"/>
+ <terraintypes>
+  <terrain name="Grass" tile="0"/>
+ </terraintypes>
+</tileset>
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject actor;
+	public float moveSpeed = 0;
+	private Vector3 actorPos;  
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		actorPos = new Vector3 (actor.transform.position.x, actor.transform.position.y, transform.position.z);
+		transform.position = Vector3.Lerp (transform.position, actorPos, moveSpeed * Time.deltaTime);
+
+	}
+}
