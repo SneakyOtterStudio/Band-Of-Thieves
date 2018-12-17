b9bd94d205763e1c2bff5d578e7fbfc0027f240f
SneakyOtterStudio <31124705+SneakyOtterStudio@users.noreply.github.com>
2018-12-17 11:55:43 -0500

    Attempting to render the Main Menu background

--- Assets/Scripts/FadeManager.cs
+++ Assets/Scripts/FadeManager.cs
@@ -1,73 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeManager : MonoBehaviour {
 
-	public float changeTimeSeconds = 5;
-	public float startAlpha = 0;
-	public float endAlpha = 1;
+	private Image image;
 
-	float changeRate = 0;
-	float timeSoFar = 0;
-	bool fading = false;
-	CanvasGroup canvasGroup;
+	void Start (){
+		image = gameObject.GetComponent<Image>();
+		StartCoroutine(FadeIn());
 
-
-	void Start()
-	{
-		canvasGroup = this.GetComponent<CanvasGroup>();
-		if(canvasGroup == null)
-		{
-			Debug.Log("Must have canvas group attached!");
-			this.enabled = false;
-		}
 	}
 
-	public void FadeIn()
-	{
-		startAlpha = 0;
-		endAlpha = 1;
-		timeSoFar = 0;
-		fading = true;
-		StartCoroutine(FadeCoroutine());
-	}
-
-	public void FadeOut()
-	{
-		startAlpha = 1;
-		endAlpha = 0;
-		timeSoFar = 0;
-		fading = true;
-		StartCoroutine(FadeCoroutine());
-	}
-
-	IEnumerator FadeCoroutine()
-	{
-		changeRate = (endAlpha - startAlpha) / changeTimeSeconds;
-		SetAlpha(startAlpha);
-		while (fading)
-		{
-			timeSoFar += Time.deltaTime;
-
-			if (timeSoFar > changeTimeSeconds)
-			{
-				fading = false;
-				SetAlpha(endAlpha);
-				yield break;
-			} else
-			{
-				SetAlpha(canvasGroup.alpha + (changeRate * Time.deltaTime));
-			}
-
+	IEnumerator FadeIn(){
+		float time = 2f;
+		Color color = image.color;
+		while (image.color.a > 0) {
+			color.a -= Time.deltaTime / time;
+			image.color = color;
 			yield return null;
 		}
 	}
 
-
-
-	public void SetAlpha(float alpha)
+	IEnumerator FadeOut()
 	{
-		canvasGroup.alpha = Mathf.Clamp(alpha, 0, 1);
+		float time = 2f;
+		Color color = image.color;
+		while(image.color.a < 1)
+		{
+			color.a += Time.deltaTime / time;
+			image.color = color;
+			yield return null;
+		}
 	}
 }
